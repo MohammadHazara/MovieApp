13e45daed1c2612cad02db6560a61ae3dd438b50
Mohamamd Hazara <moham1mad@gmail.com>
2018-02-07 03:54:42 +0100

    Adding rating attribute to movies

--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -21,10 +21,11 @@ namespace MvcMovie.Models
                 context.Movie.AddRange(
                      new Movie
                      {
-                         Title = "When Harry Met Sally",
-                         ReleaseDate = DateTime.Parse("1989-1-11"),
-                         Genre = "Romantic Comedy",
-                         Price = 7.99M
+                       Title = "When Harry Met Sally",
+                       ReleaseDate = DateTime.Parse("1989-1-11"),
+                       Genre = "Romantic Comedy",
+                       Price = 7.99M,
+                       Rating = "3*"
                      },
 
                      new Movie
@@ -32,7 +33,8 @@ namespace MvcMovie.Models
                          Title = "Ghostbusters ",
                          ReleaseDate = DateTime.Parse("1984-3-13"),
                          Genre = "Comedy",
-                         Price = 8.99M
+                         Price = 8.99M,
+                         Rating = "3*"
                      },
 
                      new Movie
@@ -40,7 +42,8 @@ namespace MvcMovie.Models
                          Title = "Ghostbusters 2",
                          ReleaseDate = DateTime.Parse("1986-2-23"),
                          Genre = "Comedy",
-                         Price = 9.99M
+                         Price = 9.99M,
+                         Rating = "3*"
                      },
 
                    new Movie
@@ -48,7 +51,8 @@ namespace MvcMovie.Models
                        Title = "Rio Bravo",
                        ReleaseDate = DateTime.Parse("1959-4-15"),
                        Genre = "Western",
-                       Price = 3.99M
+                       Price = 3.99M,
+                       Rating = "3*"
                    }
                 );
                 context.SaveChanges();
